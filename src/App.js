2a6d02dd2254db925fd4849d37c5329ecf850100
Jeff Ginn <jeff@JeffsMBP.simplerose.com>
2023-05-20 17:08:24 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. JJJ
+          Edit <code>src/App.js</code> and save to reload. JJJeeee
         </p>
         <a
           className="App-link"
